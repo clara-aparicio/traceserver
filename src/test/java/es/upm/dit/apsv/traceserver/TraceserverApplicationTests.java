ce61e133dcf20ff40fd2d8451b2637aa6650b57c
NAME SURNAME s306907 <s306907@studenti.polito.it>
2023-11-30 09:31:34 +0100

    first commit

---
+++ src/test/java/es/upm/dit/apsv/traceserver/TraceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.apsv.traceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
